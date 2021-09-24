4bd20ceae94d3a280bd67f4f9d199f5926163fed
Louis Allard <la.devweb@yahoo.com>
2021-09-24 15:41:30 +0200

    Tuto assets etc p5.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import moi from '../public/moi.jpeg'
 
 export default function Home() {
   return (
@@ -64,10 +65,11 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src={moi} alt="moi" width={100} height={100} />
           </span>
         </a>
       </footer>
     </div>
   )
 }
+
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const firstpost = () => {
 	return(
 	<>
+<Head><title>First post</title></Head>
 	<h1>First post</h1>
 	<h2>
 		<Link href="/">
